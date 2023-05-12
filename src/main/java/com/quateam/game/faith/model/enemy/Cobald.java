091978ce41c8ef826322db8b43dde9a0c2688e99
BelGray <goldendaniel186@gmail.com>
2023-05-12 16:35:21 +0300

    Изменение типа врагов

--- src/main/java/com/quateam/game/faith/model/enemy/Cobald.java
+++ src/main/java/com/quateam/game/faith/model/enemy/Cobald.java
@@ -5,7 +5,7 @@ import com.quateam.game.faith.model.Enemy;
 public class Cobald extends Enemy {
     public Cobald(int level, float healPoint, float armorPoint, float damagePoint) {
         super("Кобальд", "Кобальд - хранитель богатств темных и сырых пещер, таких же темных, " +
-                "как и его намерения. Кобальд атакует незванных гостей своей тяжелой каменной киркой", "day/night",
+                "как и его намерения. Кобальд атакует незванных гостей своей тяжелой каменной киркой", "Подземный/ближний бой",
                 "Пещеры", level, healPoint, armorPoint, damagePoint);
     }
 }
--- src/main/java/com/quateam/game/faith/model/enemy/Octopus.java
+++ src/main/java/com/quateam/game/faith/model/enemy/Octopus.java
@@ -6,7 +6,7 @@ public class Octopus extends Enemy {
     public Octopus(int level, float healPoint, float armorPoint, float damagePoint) {
         super("Осьминог", "Осьминоги - недружелюбные создания, плавающее у берегов" +
                 " королевства Фэйтмир. Любого недоброжелателя осьминог контузит и отбрасывает своими " +
-                "мощными щупальцами.", "day/night", "Пляж/Океан",
+                "мощными щупальцами.", "Подводный/ближний бой", "Пляж/Океан",
                 level, healPoint, armorPoint, damagePoint);
     }
 }
